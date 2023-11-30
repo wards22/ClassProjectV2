ec0af6903aee15c9c62d6f82e4003f3f37389cd4
CrischellePolley <150496480+CrischellePolley@users.noreply.github.com>
2023-11-29 23:54:43 -0600

    Update Main.cpp

--- ClassProject/Main.cpp
+++ ClassProject/Main.cpp
@@ -3,12 +3,10 @@
 
 int main() {
 
-<<<<<<< HEAD
     //GeneralUI::displayGeneralUI();
       TenantUI::displayTenantMenu(1);
-=======
+
     GeneralUI::displayGeneralUI();
->>>>>>> 6c5d6ac1803db2a95e4cf9291e1cbfcc573f85e2
 
     return 0;
 }
